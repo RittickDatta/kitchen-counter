4cad74e95b2f216cb7cb99a02cc6a12c1afe231b
Rittick Datta <drittick@gmail.com>
2023-11-10 16:30:43 +0000

    Colors and fonts; Content Box;

--- src/AppRoutes.tsx
+++ src/AppRoutes.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Home from "./components/Home/Home";
 import Inventory from "./components/Inventory/Inventory";
+import PageNotFound from "./components/PageNotFound/PageNotFound";
 import RecipieChecklist from "./components/RecipieChecklist/RecipieChecklist";
 import ShoppingList from "./components/ShoppingList/ShoppingList";
 
@@ -12,6 +13,7 @@ const AppRoutes = () => {
       <Route path="/Inventory" Component={Inventory} />
       <Route path="/RecipieChecklist" Component={RecipieChecklist} />
       <Route path="/ShoppingList" Component={ShoppingList} />
+      <Route path="*" Component={PageNotFound} />
     </Routes>
   );
 };
